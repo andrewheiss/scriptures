727a75d0c49f9e5b4f866997a89f931b33d157a9
Ben Crowder <ben.crowder@gmail.com>
2010-02-04 20:23:27 -0700

    Foolishly deleted footer.php while showing Git off to my wife. Replaced it.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 	<div id="nav">
-		<b>j</b> = next verse | <b>k</b> = prev verse | <b>n</b> = next chapter | <b>p</b> = prev chapter | <b>g</b> = go to location | <b>r</b> = random chapter<?php echo $pilcrow_footer; ?>
+		<b>j</b> = next verse | <b>k</b> = prev verse | <b>n</b> = next chapter | <b>p</b> = prev chapter | <b>g</b> = go to location | <b>r</b> = random chapter | <b>z</b> = toggle pilcrows
 		<div id="aboutlink">Written by <a href="http://bencrowder.net/">Ben Crowder</a> and <a href="http://imetchad.org/">Chad Hansen</a>. See the <a href="http://bencrowder.net/projects/scriptures/">project page</a> for more information.</div>
 	</div>
 </body>
