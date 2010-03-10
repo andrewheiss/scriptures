94e33f1cb14f55094daa02d0123d97837fa0b183
Ben Crowder <ben.crowder@gmail.com>
2010-03-10 10:24:29 -0700

    Fixed footer to be prettier

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 	<div id="nav">
-		<b>j</b> = next verse | <b>k</b> = prev verse | <b>n</b> = next chapter | <b>p</b> = prev chapter | <b>g</b> = go to location | <b>r</b> = random chapter | <b>z</b> = toggle pilcrows
+		<b>j</b> = next verse | <b>k</b> = prev verse | <b>n</b> = next chapter | <b>p</b> = prev chapter<br/>
+	    <b>g</b> = go to location | <b>r</b> = random chapter | <b>z</b> = toggle pilcrows
 		<div id="aboutlink">Written by <a href="http://bencrowder.net/">Ben Crowder</a> and <a href="http://imetchad.org/">Chad Hansen</a>. See the <a href="http://bencrowder.net/projects/scriptures/">project page</a> for more information.</div>
 	</div>
 </body>
